5365129fd3516c29ccdfcafc2cda6093f58ede7e
Sunil Github <github@eagle.(none)>
2011-07-01 12:43:23 +0530

    ..

---
+++ src/bitvector/core.clj
@@ -0,0 +1,1 @@
+(ns bitvector.core)
---
+++ test/bitvector/test/core.clj
@@ -0,0 +1,6 @@
+(ns bitvector.test.core
+  (:use [bitvector.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
