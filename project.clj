745f0c85ca7dae33df8686c59326708847b4ff10
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-07-01 16:39:11 +0530

    added the data files

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
 (defproject bitvector "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/data.finger-tree "0.0.1"]
+                 [clj-iterate "0.95-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
+                     [clojure-source "1.2.1"]]
+  :main bitvector.core)
+  
--- src/bitvector/core.clj
+++ src/bitvector/core.clj
@@ -1,1 +1,33 @@
-(ns bitvector.core)
+(ns bitvector.core
+  (:use iterate))
+
+(defn a-update [arr [:as keys] f]
+  (let [v (f (apply aget arr keys))]
+    (apply aset arr (conj keys v)) arr))
+        
+(defn for-each-edge
+  ([f f-arg prufer-code]
+     "calls f with f-arg as the first argument ,initially and return value of previous call to
+      f subsequently, and every edge being passed as a pair of nodes"
+     (let [n (+ (count prufer-code) 2)
+           degree (loop [deg (int-array n 1) [cur-code & rest-of-prufer-code] prufer-code]
+                    (if-not cur-code deg (recur (a-update deg [cur-code] inc) rest-of-prufer-code)))]
+       (loop [[first-degree-1-nodes & rest-of-degree-1-nodes :as degree-1-nodes] (keep-indexed #(when (= %2 1) %1) degree)
+              cur-degree degree [cur-code & rest-of-prufer-code] prufer-code cur-f-arg f-arg]
+         (if-not cur-code (f cur-f-arg degree-1-nodes)
+                 (let [nf-arg (f cur-f-arg [cur-code first-degree-1-nodes])
+                       new-degree (-> cur-degree (a-update [first-degree-1-nodes] dec) (a-update [cur-code] dec))]
+                   (recur (if (= (aget new-degree cur-code) 1) (conj rest-of-degree-1-nodes cur-code) rest-of-degree-1-nodes)
+                          new-degree rest-of-prufer-code nf-arg))))))
+  ([f prufer-code] (for-each-edge (fn [_ ed] (f ed)) nil prufer-code)))
+
+(defn read-bit-vectors [fname]
+  
+
+           
+    
+        
+        
+                   
+        
+  
